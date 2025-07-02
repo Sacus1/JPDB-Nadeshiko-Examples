a4b4d78ae08cd5dd0c5af4a7424c573cb3f3b26e
Sacus <75908397+Sacus1@users.noreply.github.com>
2025-07-02 15:37:23 +0200

    Updated version number

--- JPDB nadeshiko Examples.js
+++ JPDB nadeshiko Examples.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         JPDB Nadeshiko Examples
-// @version      1.22.3
+// @version      1.23
 // @description  Embeds anime images & audio examples into JPDB review and vocabulary pages using Nadeshiko's API. Compatible only with TamperMonkey.
 // @author       awoo& Sacus
 // @namespace    jpdb-nadeshiko-examples
